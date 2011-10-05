f79a0ed904a47bc4306aeca972d0c8c8cf8a7246
tony.bigbee <tony.bigbee@4ba58646-2788-3ad3-111f-8a13faff2449>
2011-10-05 21:20:30 +0000

    Revised classpath to use new sugarscape.jar

--- start/anim2-3.sh
+++ start/anim2-3.sh
@@ -1,3 +1,3 @@
-lib=lib/mason.jar:lib/jfreechart-1.0.13.jar:lib/jcommon-1.0.16.jar:lib/quaqua-colorchooser-only.jar:lib/itext-2.1.5.jar
+lib=sugarscape.jar:lib/mason.jar:lib/jfreechart-1.0.13.jar:lib/jcommon-1.0.16.jar:lib/quaqua-colorchooser-only.jar:lib/itext-2.1.5.jar
 
-java -classpath .:$lib sim.app.sugarscape.SugarscapeWithUIHigh -file conf/anim2-3.conf
+java -classpath $lib sim.app.sugarscape.SugarscapeWithUIHigh -file conf/anim2-3.conf
